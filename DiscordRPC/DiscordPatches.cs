9d55ce44cf2b772492a1236f90599e9e804c191d
Sekoree <me@sekoree.dev>
2022-10-02 14:39:22 +0200

    Finished initial Discord RPC patches

--- DiscordRPC/DiscordPatches.cs
+++ DiscordRPC/DiscordPatches.cs
@@ -1,54 +1,275 @@
 ﻿using System;
 using Discord;
 using HarmonyLib;
+using MelodyReactor2;
 using UnityEngine;
 
 namespace DiscordRPC
 {
+    public class Shared
+    {
+        public static Discord.Discord DiscordRpcClient { get; set; }
+    }
+
+    #region Init + Callbacks
+
     [HarmonyPatch(typeof(MGame), nameof(MGame.Initialize))]
     public class DiscordInitialize
     {
-        public static Discord.Discord DiscordRpcClient { get; set; }
-        
         public static void Postfix()
         {
             Debug.Log("Discord RPC: Initializing");
-            var clientId = "Client ID Here";
-            DiscordRpcClient = new Discord.Discord(Int64.Parse(clientId), (UInt64)Discord.CreateFlags.Default);
-            DiscordRpcClient.SetLogHook(Discord.LogLevel.Debug, (level, message) =>
-            {
-                Debug.Log($"Log {level} {message}");
-            });
-
-            var testActivity = new Activity()
-            {
-                State = "Somewhere",
-                Details = "Doing something",
-                Assets = new ActivityAssets()
-                {
-                    LargeImage = "testimage",
-                    LargeText = "Yep idk",
-                    SmallImage = "testimage",
-                    SmallText = "Hello there"
-                }
-            };
-            DiscordRpcClient.GetActivityManager().UpdateActivity(testActivity, (res) =>
-            {
-                Debug.Log($"Update {res}");
-            });
+            //replace with your discord apps client ID
+            Shared.DiscordRpcClient = new Discord.Discord(default, (UInt64)Discord.CreateFlags.Default);
+            Shared.DiscordRpcClient.SetLogHook(LogLevel.Debug,
+                (level, message) => { Debug.Log($"Log {level} {message}"); });
             Debug.Log("Discord RPC: Initialized");
         }
     }
-    
+
     [HarmonyPatch(typeof(GameController), "Update")]
     public class DiscordCallbacks
     {
         public static void Postfix()
         {
-            if (DiscordInitialize.DiscordRpcClient != null)
+            if (Shared.DiscordRpcClient != null)
             {
-                DiscordInitialize.DiscordRpcClient.RunCallbacks();
+                Shared.DiscordRpcClient.RunCallbacks();
             }
         }
     }
+
+    #endregion
+
+    #region Presence Updates
+
+    [HarmonyPatch(typeof(FileSelectUIController), "Show")]
+    public class FileSelectUIController_Show
+    {
+        public static void Postfix()
+        {
+            if (Shared.DiscordRpcClient == null) 
+                return;
+            
+            var activityManager = Shared.DiscordRpcClient.GetActivityManager();
+            var activity = new Activity
+            {
+                State = "Browsing Songs",
+                Assets =
+                {
+                    LargeImage = "melody"
+                },
+                Timestamps =
+                {
+                    Start = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                }
+            };
+            activityManager.UpdateActivity(activity, result => { });
+        }
+    }
+    
+    [HarmonyPatch(typeof(MainMenuUIController), "Show")]
+    public class MainMenuUIController_Show
+    {
+        public static void Postfix()
+        {
+            if (Shared.DiscordRpcClient == null) 
+                return;
+            
+            var activityManager = Shared.DiscordRpcClient.GetActivityManager();
+            var activity = new Activity
+            {
+                State = "In the Main Menu",
+                Assets =
+                {
+                    LargeImage = "melody"
+                },
+                Timestamps =
+                {
+                    Start = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                }
+            };
+            activityManager.UpdateActivity(activity, result => { });
+        }
+    }
+    
+    [HarmonyPatch(typeof(SettingsUIController), "Show")]
+    public class SettingsUIController_Show
+    {
+        public static void Postfix()
+        {
+            if (Shared.DiscordRpcClient == null) 
+                return;
+            
+            var activityManager = Shared.DiscordRpcClient.GetActivityManager();
+            var activity = new Activity
+            {
+                State = "Changing Settings",
+                Assets =
+                {
+                    LargeImage = "melody"
+                },
+                Timestamps =
+                {
+                    Start = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                }
+            };
+            activityManager.UpdateActivity(activity, result => { });
+        }
+    }
+    
+    [HarmonyPatch(typeof(CustomizationUIController), "Show")]
+    public class CustomizationUIController_Show
+    {
+        public static void Postfix()
+        {
+            if (Shared.DiscordRpcClient == null) 
+                return;
+            
+            var activityManager = Shared.DiscordRpcClient.GetActivityManager();
+            var activity = new Activity
+            {
+                State = "Customizing Melody",
+                Assets =
+                {
+                    LargeImage = "melody"
+                },
+                Timestamps =
+                {
+                    Start = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                }
+            };
+            activityManager.UpdateActivity(activity, result => { });
+        }
+    }
+    
+    [HarmonyPatch(typeof(EndScoreUIController), "PopulateData")]
+    public class EndScoreUIController_PopulateData
+    {
+        public static void Postfix(EndScoreUIController __instance, MusicData musicData, Track track)
+        {
+            if (Shared.DiscordRpcClient == null) 
+                return;
+            
+            var songName = __instance.TextTitle.text;
+            var activityManager = Shared.DiscordRpcClient.GetActivityManager();
+            var hearts = "";
+            if (!track.PerfectChainTargetAchieved)
+            {
+                for (var i = 0; i < 5; i++)
+                {
+                    var hitTarget = track.ScoreTargets[i];
+                    if (track.Score >= hitTarget)
+                    {
+                        hearts += "❤";
+                    }
+                    else
+                    {
+                        hearts += "🖤";
+                    }
+                }
+            }
+            else
+            {
+                hearts = "💛💛💛💛💛";
+            }
+            
+            var activity = new Activity
+            {
+                State = "Escaped : " + songName,
+                Details = $"{hearts} | Difficulty: {__instance.TextDifficultyValue.text} | Best Chain: {__instance.TextMaxChainValue.text}",
+                Assets =
+                {
+                    LargeImage = "melody"
+                },
+                Timestamps =
+                {
+                    Start = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                }
+            };
+            activityManager.UpdateActivity(activity, result => { });
+        }
+    }
+    
+    [HarmonyPatch(typeof(MusicController), "PlayPreloadedStream")]
+    public class MusicController_PlayPreloadedStream
+    {
+        public static void Postfix(MusicController __instance)
+        {
+            if (Shared.DiscordRpcClient == null) 
+                return;
+            
+            var musicInfo = MReactor.AudioEngine.MusicData.MusicInfo;
+            var activityManager = Shared.DiscordRpcClient.GetActivityManager();
+            var activity = new Activity
+            {
+                State = "Escaping : " + musicInfo.TagTitle,
+                //Details = $"Difficulty: {__instance.Track.} | Best Chain: {__instance.TrackData.BestChain}",
+                Assets =
+                {
+                    LargeImage = "melody"
+                },
+                Timestamps =
+                {
+                    End = (long) (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds +
+                        (long) musicInfo.Duration - (long) musicInfo.Position
+                }
+            };
+            activityManager.UpdateActivity(activity, result => { });
+        }
+    }
+    
+    [HarmonyPatch(typeof(MusicController), "PauseStream")]
+    public class PauseMenuUIController_Show
+    {
+        public static void Postfix()
+        {
+            if (Shared.DiscordRpcClient == null) 
+                return;
+            
+            var activityManager = Shared.DiscordRpcClient.GetActivityManager();
+            var activity = new Activity
+            {
+                State = "Paused",
+                Assets =
+                {
+                    LargeImage = "melody"
+                },
+                Timestamps =
+                {
+                    Start = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                }
+            };
+            activityManager.UpdateActivity(activity, result => { });
+        }
+    }
+    
+    [HarmonyPatch(typeof(MusicController), "ResumeStream")]
+    public class MusicController_ResumeStream
+    {
+        public static void Postfix()
+        {
+            if (Shared.DiscordRpcClient == null) 
+                return;
+            
+            var musicInfo = MReactor.AudioEngine.MusicData.MusicInfo;
+            var activityManager = Shared.DiscordRpcClient.GetActivityManager();
+            var activity = new Activity
+            {
+                State = "Escaping : " + musicInfo.TagTitle,
+                Assets =
+                {
+                    LargeImage = "melody"
+                },
+                Timestamps =
+                {
+                    End = (long) (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds +
+                        (long) musicInfo.Duration - (long) musicInfo.Position
+                }
+            };
+            activityManager.UpdateActivity(activity, result => { });
+        }
+    }
+
+    #endregion
 }
